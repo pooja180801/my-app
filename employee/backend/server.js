2081de9d221c6f8ecda18dc43acfdd12563419be
Pooja Kulachelvam <139669968+pooja180801@users.noreply.github.com>
2024-08-19 11:06:02 +0530

    Update server.js

--- employee/backend/server.js
+++ employee/backend/server.js
@@ -9,9 +9,19 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors());
 
-mongoose.connect('mongodb+srv://kulampooja:Password123@cluster0.u4mtq.mongodb.net/employee')
-.then(() => console.log('DB connected'))
-.catch((error) => console.error('DB connection error:', error));
+async function connectDB() {
+    try {
+      await mongoose.connect('mongodb+srv://kulampooja:Password123@cluster0.u4mtq.mongodb.net/employee', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('DB connected');
+    } catch (error) {
+      console.error('DB connection error:', error);
+    }
+  }
+  
+  connectDB();
 
 
 app.use('/uploads', express.static('uploads')); 
